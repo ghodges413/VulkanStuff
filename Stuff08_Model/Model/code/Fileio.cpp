f5037a87ff6207fd777710488640e0467e1d2097
ghodges413 <38189889+ghodges413@users.noreply.github.com>
2019-02-10 02:19:21 -0600

    Added model

---
+++ Stuff08_Model/Model/code/Fileio.cpp
@@ -0,0 +1,126 @@
+/*
+ *  Fileio.cpp
+ *
+ */
+
+#include "Fileio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+
+#if 1//def WINDOWS
+#include <direct.h>
+#define GetCurrentDir _getcwd
+#else
+#include <sys/file.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#define GetCurrentDir getcwd
+#endif
+
+static char g_ApplicationDirectory[ FILENAME_MAX ];	// needs to be CWD
+static bool g_WasInitialized = false;
+
+/*
+=================================
+InitializeFileSystem
+=================================
+*/
+void InitializeFileSystem() {
+	if ( g_WasInitialized ) {
+		return;
+	}
+	g_WasInitialized = true;
+
+	const bool result = GetCurrentDir( g_ApplicationDirectory, sizeof( g_ApplicationDirectory ) );
+	assert( result );
+	if ( result ) {
+		printf( "ApplicationDirectory: %s\n", g_ApplicationDirectory );
+	} else {
+		printf( "ERROR: Unable to get current working directory!\n");
+	}
+}
+
+/*
+=================================
+RelativePathToFullPath
+=================================
+*/
+void RelativePathToFullPath( const char * relativePathName, char * fullPath ) {
+	InitializeFileSystem();
+
+	sprintf( fullPath, "%s/%s", g_ApplicationDirectory, relativePathName );
+}
+
+ /*
+ ====================================================
+ GetFileData
+ Opens the file and stores it in data
+ ====================================================
+ */
+bool GetFileData( const char * fileNameLocal, unsigned char ** data, unsigned int & size ) {
+	InitializeFileSystem();
+
+	char fileName[ 2048 ];
+	sprintf( fileName, "%s/%s", g_ApplicationDirectory, fileNameLocal );
+
+	FILE *file = NULL;
+	
+	// open file for reading
+	printf( "opening file: %s\n", fileName );
+	file = fopen( fileName, "rb" );
+	
+	// handle any errors
+	if ( file == NULL ) {
+		printf("ERROR: open file failed: %s\n", fileName );
+		return false;
+	}
+	
+	// get file size
+	fseek( file, 0, SEEK_END );
+	fflush( file );
+	size = ftell( file );
+	fflush( file );
+	rewind( file );
+	fflush( file );
+	
+	// output file size
+	printf( "file size: %u\n", size );
+	
+	// create the data buffer
+	*data = (unsigned char*)malloc( ( size + 1 ) * sizeof( unsigned char ) );
+	//*data = (unsigned char*)calloc( size + 1, sizeof( unsigned char ) ); // calloc initializes memory to zero
+    
+	// handle any errors
+	if ( *data == NULL ) {
+		printf( "ERROR: Could not allocate memory!\n" );
+		fclose( file );
+		return false;
+	}
+
+	// zero out the memory
+	memset( *data, 0, ( size + 1 ) * sizeof( unsigned char ) );
+	
+	// read the data
+	unsigned int bytesRead = (unsigned int)fread( *data, sizeof( unsigned char ), size, file );
+    fflush( file );
+	printf( "total bytes read: %u\n", bytesRead );
+    
+    assert( bytesRead == size );
+	
+	// handle any errors
+	if ( bytesRead != size ) {
+		printf( "ERROR: reading file went wrong\n" );
+		fclose( file );
+		if ( *data != NULL ) {
+			free( *data );
+		}
+		return false;
+	}
+	
+	fclose( file );
+	printf( "Read file was success\n");
+	return true;
+}
---
+++ Stuff08_Model/Model/code/application.h
@@ -0,0 +1,164 @@
+//
+//  application.h
+//
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <array>
+#include <set>
+
+
+/*
+====================================================
+SwapChainSupportDetails
+====================================================
+*/
+struct SwapChainSupportDetails {
+	VkSurfaceCapabilitiesKHR capabilities;
+	std::vector< VkSurfaceFormatKHR > formats;
+	std::vector< VkPresentModeKHR > presentModes;
+};
+
+/*
+====================================================
+QueueFamilyIndices
+====================================================
+*/
+struct QueueFamilyIndices {
+	int graphicsFamily = -1;
+	int presentFamily = -1;
+
+	bool IsComplete() {
+		return graphicsFamily >= 0 && presentFamily >= 0;	// Check that it's a device that can both and draw and present it to a monitor?
+	}
+};
+
+
+/*
+====================================================
+Application
+====================================================
+*/
+class Application {
+public:
+	Application();
+	~Application();
+
+	void MainLoop();
+
+private:
+	GLFWwindow * m_glfwWindow;
+
+	VkInstance m_vkInstance;
+	VkDebugReportCallbackEXT m_vkDebugCallback;
+
+	VkSurfaceKHR m_vkSurface;
+
+	//
+	//	Device related
+	//
+	VkPhysicalDevice m_vkPhysicalDevice;
+	VkDevice m_vkDevice;
+
+	VkQueue m_vkGraphicsQueue;
+	VkQueue m_vkPresentQueue;
+
+	//
+	//	Swap chain related
+	//
+	VkSwapchainKHR m_vkSwapChain;
+	VkExtent2D m_vkSwapChainExtent;
+
+	VkFormat m_vkSwapChainColorImageFormat;
+	std::vector< VkImage > m_vkSwapChainColorImages;
+	std::vector< VkImageView > m_vkSwapChainImageViews;
+
+	VkFormat m_vkSwapChainDepthFormat;
+	VkImage m_vkSwapChainDepthImage;
+	VkImageView m_vkSwapChainDepthImageView;
+	VkDeviceMemory m_vkSwapChainDepthImageMemory;
+	
+	std::vector< VkFramebuffer > m_vkSwapChainFramebuffers;
+
+	VkRenderPass m_vkRenderPass;
+
+	//
+	//	Uniform Buffer
+	//
+	VkBuffer m_vkUniformBuffer;
+	VkDeviceMemory m_vkUniformBufferMemory;
+	VkDeviceSize m_vkUniformBufferSize;
+
+	//
+	//	Command Buffers
+	//
+	VkCommandPool m_vkCommandPool;
+	std::vector< VkCommandBuffer > m_vkCommandBuffers;
+
+	//
+	//	Texture loaded from file
+	//
+	VkFormat m_vkTextureFormat;
+	VkImage m_vkTextureImage;
+	VkImageView m_vkTextureImageView;
+	VkDeviceMemory m_vkTextureDeviceMemory;
+	VkSampler m_vkTextureSampler;
+	VkExtent2D m_vkTextureExtents;
+
+	//
+	//	Model
+	//
+	VkBuffer m_vkVertexBuffer;
+	VkDeviceMemory m_vkVertexBufferMemory;
+	VkBuffer m_vkIndexBuffer;
+	VkDeviceMemory m_vkIndexBufferMemory;
+
+
+	bool CheckValidationLayerSupport() const;
+	std::vector< const char * > GetGLFWRequiredExtensions() const;
+
+	void InitializeGLFW();
+	bool InitializeVulkan();
+	void Cleanup();
+
+	// Window or Instance Related
+	static void OnWindowResized( GLFWwindow * window, int width, int height );
+	static VKAPI_ATTR VkBool32 VKAPI_CALL DebugCallback( VkDebugReportFlagsEXT flags, VkDebugReportObjectTypeEXT objType, uint64_t obj, size_t location, int32_t code, const char * layerPrefix, const char * msg, void * userData );
+
+	// Device related
+	static bool CheckDeviceExtensionSupport( VkPhysicalDevice vkPhysicalDevice );
+	static QueueFamilyIndices FindQueueFamilies( VkPhysicalDevice vkPhysicalDevice, VkSurfaceKHR vkSurface );
+	static bool IsDeviceSuitable( VkPhysicalDevice vkPhysicalDevice, VkSurfaceKHR vkSurface );
+	static SwapChainSupportDetails QuerySwapChainSupport( VkPhysicalDevice device, VkSurfaceKHR vkSurface );
+
+	// Swap chain related
+	static VkSurfaceFormatKHR ChooseSwapSurfaceFormat( const std::vector<VkSurfaceFormatKHR> & availableFormats );
+	static VkPresentModeKHR ChooseSwapPresentMode( const std::vector< VkPresentModeKHR > availablePresentModes );
+	static VkExtent2D ChooseSwapExtent( const VkSurfaceCapabilitiesKHR & capabilities, uint32_t width, uint32_t height );
+	uint32_t FindMemoryType( uint32_t typeFilter, VkMemoryPropertyFlags properties );
+
+
+
+	static const int WINDOW_WIDTH = 1200;
+	static const int WINDOW_HEIGHT = 720;
+
+#if defined( DEBUG )
+	static const bool m_enableLayers = true;
+#else
+	static const bool m_enableLayers = false;
+#endif
+
+	static const std::vector< const char * > m_ValidationLayers;
+
+	static const std::vector< const char * > m_DeviceExtensions;
+};
